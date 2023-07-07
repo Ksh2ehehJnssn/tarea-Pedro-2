eeea1c48745da3532ab8635a441270c249fe5d33
Pedro Jossias Rivera Lopez <est.priveral@uml.edu.ni>
2023-07-06 19:36:55 -0600

    otro intento

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
 
-export default function ContainedButtons() {
+export default function add(ontainedButtons, ImageAvatars) {
   return (
     <Stack direction="row" spacing={2}>
       <Button variant="contained">Contained</Button>
       <Button variant="contained" disabled>Disabled</Button>
       <Button variant="contained" href="#contained-buttons">Link</Button>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
     </Stack>
   );
 }
